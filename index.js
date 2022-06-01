7ba9c694cce745c0e23d590a7c7165f6994f9806
+dennis kiptoo <+dennis.kiptoo@student.moringaschool.com>
2022-06-01 17:19:07 +0300

    js object

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// Write your solution in thi
+const doctor = {
+  name: "Sam",
+  streetAddress: "11 Broadway",
+};
+
+function updateDriverWithKeyAndValue(doctor, key, value) {
+  
+  return Object.assign({}, doctor, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(doctor, key, value) {
+  doctor[key] = value;
+
+  return doctor;
+}
+
+function deleteFromDriverByKey(doctor, key) {
+
+  const newObj = Object.assign({}, doctor);
+
+  delete newObj[key];
+
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(doctor, key) {
+  delete doctor[key];
+
+  return doctor;
+}
